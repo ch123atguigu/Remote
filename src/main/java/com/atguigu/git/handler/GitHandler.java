e8f7c862c4d2c710964117b5730fa239d1f6e773
陈欢 <@DESKTOP-CJ9H18I.mshome.net>
2018-07-24 18:08:12 +0800

    slove complict

--- src/main/java/com/atguigu/git/handler/GitHandler.java
+++ src/main/java/com/atguigu/git/handler/GitHandler.java
@@ -3,9 +3,7 @@ package com.atguigu.git.handler;
 public class GitHandler {
 	public static void main(String[] args) {
 		System.out.println("aaaaa");
-	}
-
-	public static void main(String[] args) {
 		System.out.println("bbbbbbb");
 	}
+	
 }
